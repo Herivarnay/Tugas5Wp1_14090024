03ada313f4e41d98125bfd40faf18c08521dd8e5
Intan Laksana Mulya <laksa.bungsu@gmail.com>
2016-04-02 19:27:18 +0700

    Aray Himpunan & Aray Multidimensi

---
+++ Tugas5wp1_14090024/Array_Himpunan.php
@@ -0,0 +1,21 @@
+html>
+<head>
+</head>
+<body>
+<h1> Array Himpunan : Union </h1>
+<?php
+$ar_a=array(1,2,5,7,8);
+print"Isi Array \$ar_a <br>";
+print_r($ar_a);
+print "<br>";
+$ar_b=array("abc",5,7,90);
+print"Isi Array \$ar_b<br>";
+print_r($ar_b);
+print"<br>";
+$gabung=array_merge($ar_a, $ar_b);
+print"Isi array \$gabung <br>" ;
+print_r($gabung);
+print"<br>";
+?>
+</body>
+</html>
---
+++ Tugas5wp1_14090024/Array_Multidimensi.php
@@ -0,0 +1,15 @@
+<title> Array Multidimensi </title>
+<?php
+echo"Tugas5WP1_14090024<br/><br/>";
+echo"Array Multidimensi<br/>";
+$provinsi = array
+(
+array("Semarang","Jawa Tengah"),
+array("Bandung","Jawa Barat"),
+array("Surabaya","Jawa Timur")
+);
+echo $provinsi[0][0]." Berada di provinsi ".$provinsi[0][1]."<br>";
+echo $provinsi[1][0]." Berada di provinsi ".$provinsi[1][1]."<br>";
+echo $provinsi[2][0]." Berada di provinsi ".$provinsi[2][1]."<br>";
+?>
+</body>
